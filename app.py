46826b0b014a18cbbd84ec5c93c0dff5f6a6d976
karuna24s <karuna24s@gmail.com>
2019-08-07 21:06:00 -0400

    Add logout route in app.py

--- app.py
+++ app.py
@@ -40,6 +40,11 @@ def userHome():
     else:
         return render_template('error.html',error = 'Unauthorized Access')
 
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
+
 @app.route('/validateLogin',methods=['POST'])
 def validateLogin():
     try:
